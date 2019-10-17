b8fe18a9ac76e9678469285370756bf974e84a62
Andrew Sooklall <andrewsooklall@gmail.com>
2019-10-17 12:29:18 -0400

    added quickDBD exports for ERD

---
+++ EmployeeSQL/QuickDBD-sql-challenge_ERD.sql
@@ -0,0 +1,71 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/LRgD3G
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR()   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "birth_date" VARCHAR(255)   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "gender" VARCHAR(20)   NOT NULL,
+    "hire_date" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" VARCHAR(255)   NOT NULL,
+    "to_date" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR()   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" VARCHAR(255)   NOT NULL,
+    "to_date" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" MONEY   NOT NULL,
+    "from_date" VARCHAR(255)   NOT NULL,
+    "to_date" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_no" INTEGER   NOT NULL,
+    "title" VARCHAR(255)   NOT NULL,
+    "from_date" VARCHAR(255)   NOT NULL,
+    "to_date" VARCHAR(255)   NOT NULL
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
